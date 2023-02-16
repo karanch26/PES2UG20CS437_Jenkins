7e9876be937a2eb4499c7c1bb700c2b2a2901b85
Karan Chouhan D <85885282+karanch26@users.noreply.github.com>
2023-02-16 09:12:33 +0530

    Create working.cpp

---
+++ working.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins Pipeline\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
